1f47de620310bf8ac7936b92780a40feb9df0eeb
tim <tim.parkerd@gmail.com>
2018-02-12 13:54:28 -0500

    starting point for generated pivot table for data analysis

--- scripts/SQLGenerator.js
+++ scripts/SQLGenerator.js
@@ -1,48 +1,33 @@
+// Pivot Table Generator
+let pivotTableGen = (rows, cols) => {
+  let alias = "abcdefghijklmnopqrstuvwxyz".split('')
 
+  // Create row (unions)
+  for (let row = 1; row <= rows; row++) {
+    // Row start...
+    console.log(`SELECT ${row} AS counter, COUNT(*) AS countOf`)
+    for (let col = 0; col < cols; col++)
+      console.log(`, ${alias[col]}.countOf`)
+    console.log(`FROM posts p`);
 
-let genSqlLess = (a, b, m) => {
-  let n = 1;
-  let mltfilter = `between ${a} and ${b - 1}`
-  if (b === 1) mltfilter = `< 1`
-  else if (a === b) mltfilter = `> ${a - 1}`
-  console.log(`select ${n} as id, count(*) from posts `)
-  console.log(`INNER JOIN (`)
-  let counter = 0
-  while (counter < 300) {
-    let str = `UNION (select ${++n}, count(*) from posts p where char_length(p.content_text) ${mltfilter} and score between ${(counter + 1)} and ${(counter + 10)})`
-    counter += 10
-    console.log(str)
-  }
-  console.log(`UNION (select ${++n}, count(*) from posts p where char_length(p.content_text) ${mltfilter} and score > 300)`);
-  console.log(`) AS ${alias[n]} ON ${alias[n]}.id = id`)
-  console.log(`p where char_length(p.content_text) ${mltfilter} and score < 1`);
+      // Create columns (inner joined tables)
+      for (let col = 0; col < cols; col++) {
+        // Start columns
+        console.log(`INNER JOIN (
+          SELECT ${row} AS tmpid,
+          COUNT(*) AS countOf
+          FROM posts p
+          WHERE CHAR_LENGTH(p.content_text) BETWEEN 1 AND 50
+          AND p.score < 1
+        ) AS ${alias[col]} ON ${alias[col]}.tmpid = ${row}`)
+      }
+      console.log(`WHERE CHAR_LENGTH(p.content_text) < 1
+      AND p.score < 1`)
+      // If we haven't reached the last row yet, add a UNION
+      if (row < rows)
+      console.log(`UNION`)
+    }
 }
 
-let genScore = () => {
-  let counter = 0
-  while (counter <= 300) {
-    console.log(`${counter} < n <= ${(counter + 10)}`)
-    counter += 10
-  }
-}
 
-
-// genScore()
-let gen = (limit, increment) => {
-  genSqlLess(1,1)
-  let counter = 1
-  while (counter < limit) {
-    genSqlLess(counter, counter + increment)
-    counter += increment
-  }
-  genSqlLess(counter, counter)
-}
-
-gen(400, 50)
-
-
-let alias = "abcdefghijklmnopqrstuvwxyz".split('')
-
-let counter = 1
-
-counter++
+pivotTableGen(4, 3)
